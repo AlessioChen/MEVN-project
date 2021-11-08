ee62e285d9c60f3a803eeb07694e410b1d0d1964
Paolo <paolodellorti@gmail.com>
2021-11-08 00:26:28 +0000

    MEVNB-6 |Connecting to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
 const server = express();
 const port = process.env.PORT ? process.env.PORT : 3000;
+const dbURI = process.env.DB_URI;
 
-server.listen(port);
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(res => {
+    server.listen(port);
+    console.log("----- DATABASE CHECK -----");
+  })
+  .catch(err => console.log(err));
 
 server.get('/', (req, res) => {
-    res.end('----- PROVA SERVER -----' );
+    res.end('----- SERVER CHECK -----' );
 });
+
